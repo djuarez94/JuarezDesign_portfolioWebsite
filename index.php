839a3e1f81d18f5535b3d969b4b8bfb749e6c95e
David Juarez <DavidJuarez@192.168.1.6>
2017-05-27 21:52:08 -0700

    began building site. Added bakcground parallax image to webDevelopment.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+	<head>
+        <meta charset="utf-8">
+        <title>Juarez Design : Home</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div class="logoHeader">
+			<img src="img/juarezDesign_Logo.png">
+		</div>
+		<div class="titles">
+			<a href="webDevelopment.php"><img src="img/developmentIcon.png"></a>
+			<a href="visualDesign.php"><img src="img/designIcon.png"></a>
+		</div>
+	</body>
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</html>
---
+++ webDevelopment.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+	<head>
+        <meta charset="utf-8">
+        <title>Web Development</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+				 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				   <span class="icon-bar"></span>
+				   <span class="icon-bar"></span>
+				   <span class="icon-bar"></span>
+				 </button>
+				 <a class="navbar-brand" href="index.php"><img src="img/juarezDesign_Logo.png" alt="Site Logo"></a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+					   <li class="active"><a href="webDevelopment.php">Web Development</a></li>
+					   <li><a href="visualDesign.php">Visual Design</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="parallaxWeb">
+
+		</div>
+	</body>
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</html>
